6bb75a7082b132949140b2cb7d1c4f28c848d84c
Marina <mymuse@mail.ru>
2026-04-07 12:34:00 +0300

    reverse array method added

--- src/App.java
+++ src/App.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+
 public class App {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        reverseArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void reverseArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
